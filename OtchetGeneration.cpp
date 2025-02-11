e5f7c5633fe34ea877c04f2c5acb7f8050dc8e28
Ershik286Labs <ershov.ershik286@gmail.com>
2025-02-12 00:54:35 +0300

    Update OtchetGeneration.cpp
    Update Codification

--- OtchetGeneration.cpp
+++ OtchetGeneration.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 string language(string ways, string n, int i, string programminglanguage) {
     string line{ "" };
-    fstream text(ways, ios::in | ios::out); //обрабатываем информацию в файле
+    fstream text(ways, ios::in | ios::out); //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅ
     if (text.is_open()) {
         vector<string> vec;
         for (int j = 1; j <= i; j++) {
@@ -34,7 +34,7 @@ string language(string ways, string n, int i, string programminglanguage) {
 string listing(string ways, string code, string n, int i) {
     code = "";
     string line{ "" }, line1{ "" };
-    fstream text(ways, ios::in | ios::out); //обрабатываем информацию в файле
+    fstream text(ways, ios::in | ios::out);
     if (text.is_open()) {
         vector<string> vec;
         for (int j = 1; j <= i; j++) {
@@ -43,48 +43,47 @@ string listing(string ways, string code, string n, int i) {
         }
     }
     else {
-        code = "<Сюда вставь код>";
-        cout << "не удалось открыть файл с путями" << endl;
+        code = "<РЎСЋРґР° РІСЃС‚Р°РІСЊ РєРѕРґ>";
+        cout << "РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р» СЃ РєРѕРґРѕРј" << endl;
         return code;
     }
     text.close();
-    fstream codes(n, ios::in | ios::out); //обрабатываем информацию в файле
+    fstream codes(n, ios::in | ios::out);
     if (codes.is_open()) {
         while (getline(codes, line1)) {
-            size_t commentPos = line1.find("//"); // Ищем позицию комментария
+            size_t commentPos = line1.find("//");
             if (commentPos != string::npos) {
-                line1 = line1.substr(0, commentPos); // Обрезаем строку до комментария
+                line1 = line1.substr(0, commentPos);
             }
-            code += line1 + "\n"; // Добавляем строку в code
+            code += line1 + "\n";
         }
     }
     else {
-        cout << "не удалось открыть файл с листингом" << endl;
+        cout << "Р¤Р°Р№Р» СЃ РєРѕРґР°РјРё РЅРµ РѕС‚РєСЂС‹С‚" << endl;
     }
 
     return code;
 }
 
 void titlePage(ofstream& file, string discipline, string numberLab, string teacher, string topic) {
-    file << "\\documentclass[oneside,a4paper,14pt]{extarticle} %размер шрифта 14" << endl;
+    file << "\\documentclass[oneside,a4paper,14pt]{extarticle} %пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 14" << endl;
     file << "\\usepackage[T1,T2A]{fontenc}" << endl;
     file << "\\usepackage[left=2.5cm,right=1.5cm,top=2cm,bottom=2cm]{geometry}" << endl;
-    file << "\\usepackage[utf8]{inputenc} % кодировка текста" << endl;
-    file << "\\usepackage[russian]{babel} % поддержка русского языка" << endl;
-    file << "\\usepackage{textcomp} % текстовые символы" << endl;
-    file << "\\usepackage{indentfirst} % корректировка отступов" << endl;
-    file << "\\usepackage{graphicx} % работа с изображениям" << endl;
+    file << "\\usepackage[utf8]{inputenc} % пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ" << endl;
+    file << "\\usepackage[russian]{babel} % пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ" << endl;
+    file << "\\usepackage{textcomp} % пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << endl;
+    file << "\\usepackage{indentfirst} % пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << endl;
+    file << "\\usepackage{graphicx} % пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << endl;
     file << "\\usepackage{mwe} % for blindtext and example - image - a in example" << endl;
     file << "\\usepackage{wrapfig}" << endl;
     file << "\\usepackage{caption}" << endl;
-    file << "\\usepackage{amsmath} % для формул и символов" << endl;
+    file << "\\usepackage{amsmath} % пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << endl;
     file << "\\usepackage{amsfonts}" << endl;
     file << "\\usepackage{amsthm}" << endl;
     file << "\\usepackage{enumitem}" << endl;
     file << "\\usepackage{minted}" << endl;
     file << "\\usepackage[all]{xy}" << endl;
     file << "\\usepackage[breaklinks]{hyperref}" << endl;
-    file << " % %размеркегляузаголоковразделов" << endl;
     file << "\\usepackage{titlesec}" << endl;
     file << "\\titleformat{\\section}" << endl;
     file << "\{\\normalsize\\bfseries}" << endl;
@@ -96,146 +95,145 @@ void titlePage(ofstream& file, string discipline, string numberLab, string teach
     file << "{\\normalsize\\bfseries}" << endl;
     file << "{\\thesubsection} {1em} {}" << endl;
     file << "" << endl;
-    file << "\\newcommand{\\solutionheading}[1] {% Кастомная команда" << endl;
+    file << "\\newcommand{\\solutionheading}[1] {% пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << endl;
     file << "    \\begin{flushleft}" << endl;
-    file << "    \\hspace* {1.25cm}\\textbf{#1}" << endl;
+    file << "    \\hspace* {1cm}\\textbf{#1}" << endl;
     file << "    \\end{flushleft}" << endl;
-    file << "    \\vspace{0.5cm}" << endl;
     file << " }" << endl;
     file << "" << endl;
-    file << "\\renewcommand\\baselinestretch{1.33}\\normalsize% межстрочный интервал" << endl;
-    file << "\\setlength{\\parindent}{1.25cm}" << endl;
+    file << "\\renewcommand\\baselinestretch{1.33}\\normalsize% пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << endl;
+    file << "\\setlength{\\parindent}{1cm}" << endl;
     file << "\\usepackage{indentfirst}" << endl;
     file << "" << endl;
     file << "\\begin{document}" << endl;
     file << "   \\newpage\\thispagestyle{empty}" << endl;
     file << "   \\begin{center}" << endl;
-    file << "\      МИНИСТЕРСТВО НАУКИ И ВЫСШЕГО ОБРАЗОВАНИЯ\\\\" << endl;
-    file << "\      РОССИЙСКОЙ ФЕДЕРАЦИИ" << endl;
-    file << "\      ФЕДЕРАЛЬНОЕ ГОСУДАРСТВЕННОЕ БЮДЖЕТНОЕ\\\\" << endl;
-    file << "\      ОБРАЗОВАТЕЛЬНОЕ" << endl;
-    file << "\      УЧРЕЖДЕНИЕ ВЫСШЕГО ОБРАЗОВАНИЯ\\\\" << endl;
-    file << "\      «ВЯТСКИЙ ГОСУДАРСТВЕННЫЙ УНИВЕРСИТЕТ»\\\\" << endl;
-    file << "\      Институт математики и информационных систем\\\\" << endl;
-    file << "\      Факультет автоматики и вычислительной техники\\\\" << endl;
-    file << "\      Кафедра электронных вычислительных машин" << endl;
+    file << "\      РњРРќРРЎРўР•Р РЎРўР’Рћ РќРђРЈРљР Р Р’Р«РЎРЁР•Р“Рћ РћР‘Р РђР—РћР’РђРќРРЇ\\\\" << endl;
+    file << "\      Р РћРЎРЎРР™РЎРљРћР™ Р¤Р•Р”Р•Р РђР¦РР" << endl;
+    file << "\      Р¤Р•Р”Р•Р РђР›Р¬РќРћР• Р“РћРЎРЈР”РђР РЎРўР’Р•РќРќРћР• Р‘Р®Р”Р–Р•РўРќРћР•\\\\" << endl;
+    file << "\      РћР‘Р РђР—РћР’РђРўР•Р›Р¬РќРћР•" << endl;
+    file << "\      РЈР§Р Р•Р–Р”Р•РќРР• Р’Р«РЎРЁР•Р“Рћ РћР‘Р РђР—РћР’РђРќРРЇ\\\\" << endl;
+    file << "\      В«Р’РЇРўРЎРљРР™ Р“РћРЎРЈР”РђР РЎРўР’Р•РќРќР«Р™ РЈРќРР’Р•Р РЎРРўР•РўВ»\\\\" << endl;
+    file << "\      РРЅСЃС‚РёС‚СѓС‚ РјР°С‚РµРјР°С‚РёРєРё Рё РёРЅС„РѕСЂРјР°С†РёРѕРЅРЅС‹С… СЃРёСЃС‚РµРј\\\\" << endl;
+    file << "\      Р¤Р°РєСѓР»СЊС‚РµС‚ Р°РІС‚РѕРјР°С‚РёРєРё Рё РІС‹С‡РёСЃР»РёС‚РµР»СЊРЅРѕР№ С‚РµС…РЅРёРєРё\\\\" << endl;
+    file << "\      РљР°С„РµРґСЂР° СЌР»РµРєС‚СЂРѕРЅРЅС‹С… РІС‹С‡РёСЃР»РёС‚РµР»СЊРЅС‹С… РјР°С€РёРЅ" << endl;
     file << "   \\end{center}" << endl;
     file << "   \\vspace{20mm}" << endl;
     file << "" << endl;
     file << "   \\begin{center}" << endl;
     file << "   " << numberLab << endl;
-    file << "   по дисциплине\\\\" << endl;
+    file << "   РїРѕ РґРёСЃС†РёРїР»РёРЅРµ\\\\" << endl;
     file << "   " << discipline << "\\\\" << endl;
-    file << "   " << topic << endl; //тема не работает, так как не вводится руссий язык в консоль :(
+    file << "   <<" << topic << ">>" << endl;
     file << "\  \\end{center}" << endl;
     file << "\\vspace{48mm}" << endl;
     file << "" << endl;
-    file << "\  Выполнил студент гр.ИВТб-1304-05-00 \\hspace{10mm} \\rule[-0,5mm]{30mm} {0.15mm}\\, /Ершов Н.А./" << endl;
+    file << "\  Р’С‹РїРѕР»РЅРёР» СЃС‚СѓРґРµРЅС‚ РіСЂ.РР’РўР±-1304-05-00 \\hspace{10mm} \\rule[-0,5mm]{30mm} {0.15mm}\\, /Р•СЂС€РѕРІ Рќ.Рђ./" << endl;
     file << "" << endl;
     file << "" << endl;
-    file << "\  Проверил старший преподаватель \\hfill\\rule[-0,5mm]{30mm} {0.15mm}\\, /" << teacher << "/" << endl;
+    file << "\  РџСЂРѕРІРµСЂРёР» СЃС‚Р°СЂС€РёР№ РїСЂРµРїРѕРґР°РІР°С‚РµР»СЊ \\hfill\\rule[-0,5mm]{30mm} {0.15mm}\\, /" << teacher << "/" << endl;
     file << "" << endl;
     file << "\\vfill " << endl;
     file << "\  \\begin{center}" << endl;
-    file << "\      Киров\\\\" << endl;
-    file << "\      2024" << endl;
+    file << "\      РљРёСЂРѕРІ\\\\" << endl;
+    file << "\      2025" << endl;
     file << "\  \\end{center}" << endl;
     file << "\\newpage\\thispagestyle{empty}" << endl;
 }
-int main()
-{
-    setlocale(LC_ALL, "Russian");
+int main(){
     int k = 0;
-    int sumk = 0; //копим суммы длинн кодов
+    int sumk = 0;
     int numberOfTasks;
-    string discipline{ "" }, numberLab{ "" }, task{ "" }, programminglanguage{ "" }, teacher{ "" }, topic{ "<Сюда введи тему>" };
+    string discipline{ "" }, numberLab{ "" }, task{ "" }, programminglanguage{ "" }, teacher{ "" }, topic{ "<РЎСЋРґР° РІСЃС‚Р°РІСЊ С‚РµРјСѓ>" };
     string code{ "" }, text1{ "" }, n{ "" }, report{ "" };
 
-    cout << "Введите путь к файлу с отчетом:" << endl;
+    cout << "Р’РІРµРґРёС‚Рµ РїСѓС‚СЊ Рє РёС‚РѕРіРѕРІРѕРјСѓ С„Р°Р№Р»Сѓ" << endl;
     cin >> report;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     ofstream file(report, ios::app);
     if (file.is_open()) {
-        cout << "Введите дисциплину (I/P/Cult) (I - Информатика, P - Программирование, Cult - ОИК): "; //дисциплина
+        cout << "Р’С‹Р±РµСЂРёС‚Рµ РґРёСЃС†РёРїР»РёРЅСѓ (I/P/Cult) (I - РРЅС„РѕСЂРјР°С‚РёРєР°, P - РџСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёРµ, Cult - РРЅР¶РµРЅРµСЂРЅР°СЏ РєСѓР»СЊС‚СѓСЂР°): ";
         cin >> discipline;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         if (discipline == "I") {
-            discipline = "Информатика";
-            teacher = "Шмакова Н. А.";
+            discipline = "<<РРЅС„РѕСЂРјР°С‚РёРєР°>>";
+            teacher = "РЁРјР°РєРѕРІР° Рќ. Рђ.";
         }
         if (discipline == "P") {
-            discipline = "Программирование";
-            teacher = "Крутиков А. К.";
+            discipline = "<<РџСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёРµ>>";
+            teacher = "РљСЂСѓС‚РёРєРѕРІ Рђ. Рљ.";
         }
         if (discipline == "Cult") {
-            discipline = "Основы инженерной Культуры";
-            teacher = "Скворцов А. А.";
+            discipline = "<<РРЅР¶РµРЅРµСЂРЅР°СЏ РєСѓР»СЊС‚СѓСЂР°>>";
+            teacher = "РЎРєРІРѕСЂС†РѕРІ Рў. Рќ.";//С‚СЋР±РёРє РЅРёРєРѕР»Р°РµРІРёС‡
         }
         cout << discipline << endl;
 
-        cout << "Введите номер лабораторной работы: " << endl;//номер работы
+        cout << "Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ Р»Р°Р±РѕСЂР°С‚РѕСЂРЅРѕР№ СЂР°Р±РѕС‚С‹: " << endl;//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
         cin >> numberLab;
-        numberLab = "Отчёт по лабораторной работе №" + numberLab + "\\\\";
+        numberLab = "РћС‚С‡С‘С‚ РїРѕ Р»Р°Р±РѕСЂР°С‚РѕСЂРЅРѕР№ СЂР°Р±РѕС‚Рµ в„–" + numberLab + "\\\\";
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         titlePage(file, discipline, numberLab, teacher, topic);
 
-        file << "\\solutionheading{Цель}" << endl;
-        file << "<Сюда введи цель>" << endl;
+        file << "\\solutionheading{Р¦РµР»СЊ}" << endl;
+        string purpose;
+        cout << "Р’РІРµРґРёС‚Рµ С†РµР»СЊ Р›Р°Р±РѕСЂР°С‚РѕСЂРЅРѕР№ СЂР°Р±РѕС‚С‹: " << endl;
+        cin >> purpose;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        file << "" << endl;
+        file << purpose << endl;
 
-        //собираем задание
-        file << "\\solutionheading{Задание}" << endl;
-        cout << "Введите кол-во заданий: " << endl;
+        file << "\\solutionheading{Р—Р°РґР°РЅРёРµ}" << endl;
+        cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ Р·Р°РґР°РЅРёР№: " << endl;
         cin >> numberOfTasks;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        task = "\\item";
 
-        for (int i = 0; i < numberOfTasks; i++) { //цикл идет, пока не кончится кол-во заданий
-            file << "\\textbf{Задание " << i << "}" << endl;
+        for (int i = 0; i < numberOfTasks; i++) {
             file << task << endl;
+            file << "" << endl;
         }
 
-        //заполняем решение
         file << "" << endl;
         file << "\\newpage\\thispagestyle{empty}" << endl;
-        file << "\\solutionheading{Решение}" << endl;
+        file << "\\solutionheading{Р—Р°РґР°РЅРёРµ}" << endl;
         file << "" << endl;
 
-        cout << "Введите ссылку на файл с путями (C:/Users...) !без кириллицы)" << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Р’РІРµРґРёС‚Рµ РїСѓС‚СЊ Рє С„Р°Р№Р»Сѓ СЃ РїСѓС‚СЏРјРё (C:/Users...)" << endl;
         string ways{ "" };
         cin >> ways;
 
-        fstream text(ways, ios::in | ios::out); //обрабатываем информацию в файле
+        fstream text(ways, ios::in | ios::out);
         if (text.is_open()) {
             k = 1;
         }
         else {
             k = 0;
-            cout << "не удалось открыть файл с путями" << endl;
+            cout << "РЅРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р»" << endl;
         }
         text.close();
 
         for (int i = 1; i <= numberOfTasks; i++) {
             if (i == 1) {
-                file << "\\textbf{Задание " << i << "}" << endl;
+                file << "\\textbf{Р—Р°РґР°РЅРёРµ  " << i << "}" << endl;
             }
             else {
-                file << "\\solutionheading{Задание " << i << "}" << endl;
+                file << "\\solutionheading{Р—Р°РґР°РЅРёРµ " << i << "}" << endl;
             }
-            file << "" << endl;
-            file << "\\textbf{Сюда задание}" << endl;
 
-            file << "" << endl;
-            file << "\\textbf{На входе:}\\\\" << endl;
-            file << "" << endl;
-            file << "\\textbf{На выходе:}\\\\" << endl;
-            file << "" << endl;
-            file << "\\textbf{Примеры:}\\\\" << endl;
+            file << "\\textbf{РЎСЋРґР° РІСЃС‚Р°РІСЊ Р·Р°РґР°РЅРёРµ}" << endl;
 
             file << "" << endl;
 
-            file << "\\textbf{Объяснение решения задачи: }" << endl;
+            file << "\\textbf{Р РµС€РµРЅРёРµ}" << endl;
+
+            file << "\\newpage\\thispagestyle{empty}" << endl;
+
+            file << "<РЎСЋРґР° РєР°СЂС‚РёРЅРєСѓ СЃРѕ СЃС…РµРјРѕР№>" << endl;
 
             file << "" << endl;
 
@@ -247,17 +245,7 @@ int main()
 
             programminglanguage = language(ways, n, i, programminglanguage);
 
-            file << "\\newpage\\thispagestyle{empty}" << endl;
-
-            file << "" << endl;
-
-            file << "<Здесь картинка со схемой>" << endl;
-
-            file << "" << endl;
-
-            file << "\\newpage\\thispagestyle{empty}" << endl;
-
-            file << "\\textbf{Решение на языке" << "<<" << programminglanguage << ">>" << "}" << endl;
+            file << "\\textbf{Р РµС€РµРЅРёРµ РЅР° СЏР·С‹РєРµ РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ " << "<<" << programminglanguage << ">>" << "}" << endl;
 
             file << "\\begin{minted}[linenos=true]{" << programminglanguage << "}" << endl;
 
@@ -266,17 +254,21 @@ int main()
             file << "\\end{minted}" << endl;
 
             file << "" << endl;
+
+            file << "\\newpage\\thispagestyle{empty}" << endl;
+
+            file << "" << endl;
         }
-        file << "\\solutionheading{Вывод}" << endl;
-        file << "<Здесь вывод>" << endl;
+        file << "\\solutionheading{Р’С‹РІРѕРґ}" << endl;
+        file << "<РЎСЋРґР° РІСЃС‚Р°РІСЊ РІС‹РІРѕРґ>" << endl;
         file << "\\end{document}" << endl;
 
         file.close();
     }
     else {
-        cout << "Не удалось открыть файл, попробуйте снова.";
+        cout << "РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ РёС‚РѕРіРѕРІС‹Р№ С„Р°Р№Р»";
         return 0;
     }
-    cout << "Отчет сформирован" << endl;
+    cout << "РћС‚С‡РµС‚ СЃС„РѕСЂРјРёСЂРѕРІР°РЅ" << endl;
     return 0;
 }
