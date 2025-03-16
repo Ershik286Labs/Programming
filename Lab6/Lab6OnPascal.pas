0efa707d071588327d72aad26814bf23955e2675
Ershik286Labs <ershov.ershik286@gmail.com>
2025-03-16 14:51:42 +0300

    Add files via upload

---
+++ Lab6/Lab6OnPascal.pas
@@ -0,0 +1,65 @@
+﻿Uses Crt, GraphABC;
+
+Const
+  u = 10;
+  p = 5;
+
+Var
+  i: Integer;
+  size: integer; // Глубина рекурсии
+
+type
+  Point = record
+    x, y: Double;
+  end;
+
+procedure hilbertCurve(var points: array of Point; x, y, xi, xj, yi, yj: Double; n: Integer);
+var
+  x1, y1: Double;
+begin
+  if n <= 0 then
+  begin
+    x1 := x + (xi + yi) / 2;
+    y1 := y + (xj + yj) / 2;
+    SetLength(points, Length(points) + 1);
+    points[High(points)].x := x1;
+    points[High(points)].y := y1;
+  end
+  else
+  begin
+    hilbertCurve(points, x, y, yi / 2, yj / 2, xi / 2, xj / 2, n - 1);
+    hilbertCurve(points, x + xi / 2, y + xj / 2, xi / 2, xj / 2, yi / 2, yj / 2, n - 1);
+    hilbertCurve(points, x + xi / 2 + yi / 2, y + xj / 2 + yj / 2, xi / 2, xj / 2, yi / 2, yj / 2, n - 1);
+    hilbertCurve(points, x + xi / 2 + yi, y + xj / 2 + yj, -yi / 2, -yj / 2, -xi / 2, -xj / 2, n - 1);
+  end;
+end;
+
+procedure drawHilbertCurve(const points: array of Point);
+var
+  i: Integer;
+begin
+  if Length(points) = 0 then Exit;
+
+  MoveTo(Round(points[0].x), Round(points[0].y));
+  for i := 1 to High(points) do  // исправлено: начинаем с 1, чтобы не повторять первую точку
+  begin
+    LineTo(Round(points[i].x), Round(points[i].y));
+  end;
+end;
+
+var
+  points: array of Point;
+
+Begin
+  SetWindowCaption('Фракталы: Кривая Гильберта');
+  SetWindowSize(500, 500);
+  ClearWindow;
+  Writeln('Введите глубину рекурсии');
+  readln(size);
+
+  SetLength(points, 0);
+  hilbertCurve(points, 80, 480, 400, 0, 0, -400, size);  // Исправлено: y = 480 чтобы кривая помещалась, а yj = -400 чтобы кривая была направлена вверх. уменьшил другие параметры чтобы кривая помещалась
+
+  drawHilbertCurve(points);
+  Readln;
+End.
