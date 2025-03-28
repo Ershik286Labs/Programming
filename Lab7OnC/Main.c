96a95078d2c45f40f409f6bf7b900748fc1c2889
Ershik286Labs <ershov.ershik286@gmail.com>
2025-03-28 18:31:12 +0300

    Update Main.c

--- Lab7OnC/Main.c
+++ Lab7OnC/Main.c
@@ -7,11 +7,26 @@
 
 const int maxLenCommand = 100;
 
+const int keyUp = 72;
+const int keyDown = 80;
+
 struct Dek {
     int number;
     int* leftEnd;
     int* rightEnd;
 };
+
+char MassiveHistoryCommand[40][100];
+
+int countHistoryCommand = 0;
+
+void HistoryCommand (char* command[]) {
+    countHistoryCommand = countHistoryCommand > 39 ? 0 : countHistoryCommand + 1;
+    for (int i = 1; i < countHistoryCommand + 1; i++) {
+        strcpy(MassiveHistoryCommand[i], MassiveHistoryCommand[i - 1]);
+    }
+    strcpy(MassiveHistoryCommand[0], command);
+}
 
 //PushBack — добавление в конец очереди; 2
 //PushFront — добавление в начало очереди; 2
@@ -22,6 +37,7 @@ struct Dek {
 //Swap - перестановка
 //replace - изменение значения 
 
+const char* help = "/help";
 const char* pushBack = "/pushBack";
 const char* pushFront = "/pushFront";
 const char* popBack = "/popBack";
@@ -41,6 +57,49 @@ const int Tab = 9;
 const int Enter = 13;
 const int BackSpace = 8;
 
+int choiseCommand = 0;
+
+void clear(int* len, struct Dek** dek) {
+    struct Dek* tempMassive = (struct Dek*)malloc((*len - 1) * sizeof(struct Dek));
+    if (ChoiseIndex == 0) { //Choise start element
+        for (int i = 0; i < *len - 1; i++) {
+            (*dek)[i].number = (*dek)[i + 1].number;
+            tempMassive[i].number = (*dek)[i].number;
+        }
+        *len = *len - 1;
+        *dek = (struct Dek*)malloc((*len) * sizeof(struct Dek));
+        for (int i = 0; i < *len - 1; i++) {
+            (*dek)[i].number = tempMassive[i].number;
+            (*dek)[i].leftEnd = 0;
+            (*dek)[i].rightEnd = *len - 1;
+        }
+    }
+    if (ChoiseIndex == len - 1) { //Choise end element
+        for (int i = 0; i < *len - 1; i++) {
+            tempMassive[i].number = (*dek)[i].number;
+        }
+        *len = *len - 1;
+        *dek = (struct Dek*)malloc((*len) * sizeof(struct Dek));
+        for (int i = 0; i < *len - 1; i++) {
+            (*dek)[i].number = tempMassive[i].number;
+            (*dek)[i].leftEnd = 0;
+            (*dek)[i].rightEnd = *len - 1;
+        }
+    }
+}
+
+void Help() {
+    printf("%s - показать все возможные команды\n", help);
+    printf("%s - положить элемент в конец дека\n", pushBack);
+    printf("%s - положить элемент в начало дека\n", pushFront);
+    printf("%s - взять элемент в конце дека\n", popBack);
+    printf("%s - взять элемент в начале дека\n", popFront);
+    printf("%s - проверить наличие элемента в деке\n", isEmpty);
+    printf("%s - удалить выбранный элемент\n", clear);
+    printf("%s - переставить элементы\n", swap);
+    printf("%s - заменить значение элемента дека\n", replace);
+}
+
 void Replace(struct Dek* dek, int ChoiseIndex, int *len) {
     int number;
     printf("Введите новое значение элемента стека: %d", &number);
@@ -51,7 +110,7 @@ void Swap(struct Dek* dek, int tempIndex, int ChoiseIndex) {
     int temp = dek[tempIndex].number;
     dek[tempIndex].number = dek[ChoiseIndex].number;
     dek[ChoiseIndex].number = temp;
-    printf("элементы поменяны\n");
+    printf("Элементы поменяны\n");
 }
 
 void PushBack(int* len, struct Dek** dek) {
@@ -59,16 +118,17 @@ void PushBack(int* len, struct Dek** dek) {
     printf("Введите число: ");
     scanf("%d", &number);
 
-    struct Dek* tempMassive = (struct Dek*)malloc(*len * sizeof(struct Dek));
+    struct Dek* tempMassive = (struct Dek*)malloc(*(len) * sizeof(struct Dek));
     for (int i = 0; i < *len; i++) {
-        tempMassive[i] = (*dek)[i];
+        tempMassive[i].number = (*dek)[i].number;
     }
-    free(*dek);
+    //free(*dek);
     *dek = (struct Dek*)malloc((*len + 1) * sizeof(struct Dek));
     for (int i = 0; i < *len; i++) {
-        (*dek)[i] = tempMassive[i];
+        (*dek)[i].number = tempMassive[i].number;
     }
     (*dek)[*len].number = number;
+    //printf(" введенное число: / %d, %d\n", (*dek)[*len].number, *len);
     (*len)++;
     free(tempMassive);
 }
@@ -80,7 +140,7 @@ void PushFront(int* len, struct Dek** dek) {
 
     struct Dek* tempMassive = (struct Dek*)malloc(*len * sizeof(struct Dek));
     for (int i = 0; i < *len; i++) {
-        tempMassive[i] = (*dek)[i];
+        tempMassive[i].number = (*dek)[i].number;
     }
     free(*dek);
     *dek = (struct Dek*)malloc((*len + 1) * sizeof(struct Dek));
@@ -98,9 +158,7 @@ void PopBack(int len, struct Dek* dek) {
         tempChoise = ChoiseIndex;
         ChoiseFlag = 1;
     }
-    else {
-        Swap(dek, tempChoise, ChoiseIndex);
-    }
+    if (ChoiseFlag == 1) ChoiseFlag = 2;
 }
 
 void PopFront(int len, struct Dek* dek) {
@@ -109,9 +167,7 @@ void PopFront(int len, struct Dek* dek) {
         tempChoise = ChoiseIndex;
         ChoiseFlag = 1;
     }
-    else {
-        Swap(dek, tempChoise, ChoiseIndex);
-    }
+    if (ChoiseFlag == 1) ChoiseFlag = 2;
 }
 
 void IsEmpty(int len, struct Dek* dek) {
@@ -151,6 +207,7 @@ int AutoSubstitution(char* command, int count, int NumberCommand) {
     case 4: Function = isEmpty; break;
     case 5: Function = clear; break;
     case 6: Function = swap; break;
+    case 7: Function = help; break;
     default: return 0;
     }
 
@@ -173,6 +230,23 @@ void Input(int* len, struct Dek** dek) {
     //printf("Введите команду: ");
     while (1) {
         char k = getch();
+        //printf("%d", (int)k);
+        if ((int)k == keyUp) {
+            system("cls");
+            choiseCommand = (choiseCommand + 1) % 40;
+            strcpy(command, MassiveHistoryCommand[choiseCommand]);
+            PrintMassive(*len, *dek);
+            printf("%s", command);
+        }
+        if ((int)k == keyDown) {
+            system("cls");
+            choiseCommand--;
+            choiseCommand = choiseCommand < 0 ? 0 : choiseCommand;
+            strcpy(command, MassiveHistoryCommand[choiseCommand]);
+            PrintMassive(*len, *dek);
+            printf("%s", command);
+        }
+
         if ((int)k == BackSpace) {
             if (count > 0) {
                 count--;
@@ -183,10 +257,10 @@ void Input(int* len, struct Dek** dek) {
             }
         }
         else if ((int)k == Tab) {
-            int massiveSubstitution[7];
+            int massiveSubstitution[8];
             int max = 0;
             int maxIndex = 0;
-            for (int i = 0; i < 7; i++) {
+            for (int i = 0; i < 8; i++) {
                 massiveSubstitution[i] = AutoSubstitution(command, count, i);
                 if (massiveSubstitution[i] > max) {
                     max = massiveSubstitution[i];
@@ -203,6 +277,7 @@ void Input(int* len, struct Dek** dek) {
             case 4: fullCommand = isEmpty; break;
             case 5: fullCommand = clear; break;
             case 6: fullCommand = swap; break;
+            case 7: fullCommand = help; break;
             default: fullCommand = ""; break;
             }
 
@@ -215,36 +290,53 @@ void Input(int* len, struct Dek** dek) {
         else if ((int)k == Enter) {
             printf("\n");
             if (strcmp(command, pushBack) == 0) {
+                HistoryCommand(command);
                 PushBack(len, dek);
             }
             else if (strcmp(command, pushFront) == 0) {
+                HistoryCommand(command);
                 PushFront(len, dek);
             }
             else if (strcmp(command, popBack) == 0) {
+                HistoryCommand(command);
                 PopBack(*len, *dek);
             }
             else if (strcmp(command, popFront) == 0) {
+                HistoryCommand(command);
                 PopFront(*len, *dek);
             }
             else if (strcmp(command, isEmpty) == 0) {
+                HistoryCommand(command);
                 IsEmpty(*len, *dek);
             }
             else if (strcmp(command, clear) == 0) {
+                HistoryCommand(command);
                 Clear(*len, *dek);
             }
             else if (strcmp(command, swap) == 0) {
-                if (ChoiseFlag == 1) {
+                if (ChoiseFlag == 2) {
+                    HistoryCommand(command);
                     Swap(*dek, tempChoise, ChoiseIndex);
                     ChoiseFlag = 0;
+                }
+                else {
+                    printf("сначала выберите 2 элемента\n");
                 }
             }
             else if (strcmp(command, replace) == 0) {
                 if (ChoiseFlag == 1) {
+                    HistoryCommand(command);
                     Replace(*dek, ChoiseIndex, *len);
                 }
             }
+            else if (strcmp(command, help) == 0) {
+                printf("\n");
+                Help();
+                PrintMassive(*len, *dek);
+                //printf("%s", command);
+            }
             else {
-                printf("Неизвестная команда\n");
+                printf("Неизвестная команда");
             }
             break;
         }
@@ -258,11 +350,8 @@ void Input(int* len, struct Dek** dek) {
 }
 
 int main() {
-    int len = 1;
-    struct Dek* dek = (struct Dek*)malloc(len * sizeof(struct Dek));
-    dek[0].number = 0;
-    dek[0].leftEnd = NULL;
-    dek[0].rightEnd = NULL;
+    int len = 0;
+    struct Dek* dek = (struct Dek*)malloc( len * sizeof(struct Dek));
 
     while (1) {
         Input(&len, &dek);
