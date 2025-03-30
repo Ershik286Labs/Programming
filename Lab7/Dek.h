661f6b5b4a2b7e197f09fe663d51ab97af2a139e
Ershik286Labs <ershov.ershik286@gmail.com>
2025-03-30 14:27:51 +0300

    Update Dek.h

--- Lab7/Dek.h
+++ Lab7/Dek.h
@@ -15,18 +15,15 @@ extern const char* popBack;
 extern const char* popFront;
 extern const char* isEmpty;
 extern const char* clear;
-extern const char* swap;
-extern const char* replace;
+extern const char* list;
 
 // Объявления функций
-void Clear(int* len, struct Dek** dek, int ChoiseIndex);
-void Replace(struct Dek* dek, int ChoiseIndex, int* len);
-void Swap(struct Dek* dek, int tempIndex, int ChoiseIndex);
 void PushBack(int* len, struct Dek** dek);
 void PushFront(int* len, struct Dek** dek);
-void PopBack(int len, struct Dek* dek, int ChoiseIndex, int ChoiseFlag, int tempChoise);
-void PopFront(int len, struct Dek* dek, int ChoiseIndex, int ChoiseFlag, int tempChoise);
+void PopBack(int* len, struct Dek* dek);
+void PopFront(int* len, struct Dek** dek);
 void IsEmpty(int len, struct Dek* dek);
 void PrintMassive(int len, struct Dek* dek);
+void Clear(struct Dek** dek, int* len);
 
 #endif
