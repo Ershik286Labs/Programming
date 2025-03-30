1dfe4e43b51ffce04f941b517828ba3c57b017c4
Ershik286Labs <ershov.ershik286@gmail.com>
2025-03-30 14:27:32 +0300

    Update Dek.c

--- Lab7/Dek.c
+++ Lab7/Dek.c
@@ -6,60 +6,6 @@
 
 const int CommandQuantity = 7;
 
-void Clear(int* len, struct Dek** dek, int ChoiseIndex) {
-    int size = *len - 1;
-
-    struct Dek* tempMassive = (struct Dek*)malloc((*len) * sizeof(struct Dek));
-    if (ChoiseIndex == 0) { // Choise start element
-        for (int i = 0; i < *len; i++) {
-            tempMassive[i].number = (*dek)[i].number;
-        }
-        (*len)--; // Decrement len *before* reallocation
-        if (*len > 0) { // Only reallocate if len > 0, otherwise you'll malloc(0)
-            struct Dek* newDek = (struct Dek*)malloc((*len) * sizeof(struct Dek));
-            for (int i = 0; i < *len; i++) { //Changed from *len -1 to *len
-                newDek[i].number = tempMassive[i + 1].number;
-                newDek[i].leftEnd = 0;
-                newDek[i].rightEnd = *len - 1;
-            }
-            free(*dek); // Free the *old* dek before reassigning
-            *dek = newDek;
-        }
-        else {
-            free(*dek);
-        }
-
-    }
-    else if (ChoiseIndex == (*len - 1)) { // Choise end element
-        for (int i = 0; i < *len; i++) {
-            tempMassive[i].number = (*dek)[i].number;
-        }
-        printf("%d", size);
-        (*len)--; // Decrement len *before* reallocation
-        *dek = (struct Dek*)malloc((*len) * sizeof(struct Dek));
-
-        for (int i = 0; i < *len; i++) {
-            (*dek)[i].number = tempMassive[i].number;
-            (*dek)[i].leftEnd = 0;
-            (*dek)[i].rightEnd = *len - 1;
-        }
-    }
-    free(tempMassive);
-}
-
-void Replace(struct Dek* dek, int ChoiseIndex, int* len) {
-    int number;
-    printf("Введите новое значение элемента стека: %d", &number);
-    dek[ChoiseIndex].number = number;
-}
-
-void Swap(struct Dek* dek, int tempIndex, int ChoiseIndex) {
-    int temp = dek[tempIndex].number;
-    dek[tempIndex].number = dek[ChoiseIndex].number;
-    dek[ChoiseIndex].number = temp;
-    printf("Элементы поменяны\n");
-}
-
 void PushBack(int* len, struct Dek** dek) {
     int number;
     printf("Введите число: ");
@@ -103,23 +49,41 @@ void PushFront(int* len, struct Dek** dek) {
     free(tempMassive);
 }
 
-void PopBack(int len, struct Dek* dek, int ChoiseIndex, int ChoiseFlag, int tempChoise) {
-    ChoiseIndex = len - 1;
-    if (ChoiseFlag == 0) {
-        tempChoise = ChoiseIndex;
-        ChoiseFlag = 1;
-    }
-    else if (ChoiseFlag == 1) ChoiseFlag = 2;
-    printf("%d, %d", ChoiseIndex, ChoiseFlag);
-}
+void PopBack(int* len, struct Dek* dek) {
+    int size = *len - 1;
 
-void PopFront(int len, struct Dek* dek, int ChoiseIndex, int ChoiseFlag, int tempChoise) {
-    ChoiseIndex = 0;
-    if (ChoiseFlag == 0) {
-        tempChoise = ChoiseIndex;
-        ChoiseFlag = 1;
+    struct Dek* tempMassive = (struct Dek*)malloc((*len) * sizeof(struct Dek));
+    for (int i = 0; i < *len; i++) {
+        tempMassive[i].number = (dek)[i].number;
     }
-    else if (ChoiseFlag == 1) ChoiseFlag = 2;
+    //printf("%d", size);
+    (*len)--;
+    dek = (struct Dek*)malloc((*len) * sizeof(struct Dek));
+
+    for (int i = 0; i < *len; i++) {
+        (dek)[i].number = tempMassive[i].number;
+        (dek)[i].leftEnd = 0;
+        (dek)[i].rightEnd = *len - 1;
+    }
+    free(tempMassive);
+}
+void PopFront(int* len, struct Dek** dek) {
+    int size = *len - 1;
+    struct Dek* tempMassive = (struct Dek*)malloc((*len) * sizeof(struct Dek));
+    for (int i = 0; i < *len; i++) {
+        tempMassive[i].number = (*dek)[i].number;
+    }
+    (*len)--;
+    struct Dek* newDek = (struct Dek*)malloc((*len) * sizeof(struct Dek));
+
+    for (int i = 0; i < *len; i++) {
+        newDek[i].number = tempMassive[i + 1].number;
+        newDek[i].leftEnd = 0;
+        newDek[i].rightEnd = *len - 1;
+    }
+    free(*dek);  // Освобождаем старую память, на которую указывал dek в вызывающем коде
+    *dek = newDek; // Теперь dek указывает на новый массив
+    free(tempMassive); // Освобождаем временный массив
 }
 
 void IsEmpty(int len, struct Dek* dek) {
@@ -133,6 +97,11 @@ void IsEmpty(int len, struct Dek* dek) {
         }
     }
     printf("Выбранного числа нет в структуре.\n");
+}
+
+void Clear(struct Dek** dek, int* len) {
+    *len = 0;
+    *dek = (struct Dek*)malloc((*len) * sizeof(struct Dek));
 }
 
 void PrintMassive(int len, struct Dek* dek) {
